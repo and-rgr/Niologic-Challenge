1c980a671fb477da72cb6d1203284d42deed111e
and-rgr <97305224+and-rgr@users.noreply.github.com>
2022-09-29 02:05:38 +0300

    Update closure_tables.py
    Optimized code, by removing one loop from the "extract_paths" function, and added comments

--- closure_tables.py
+++ closure_tables.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# This script expects the following 3 command line arguments to run:
-#     1 - file_name: the name of the Excel file to read 
-#     2 - db_name: the name of the database that we will connect to
-#     3 - table_name: the name of the table that we will create
 
+## SCRIPT EXECUTION ##
+
+"""
+The script can be run from the command line, using the following 3 command line arguments, in order:
+
+    file_name:  the name of the Excel file to read
+    db_name:    the name of the database that we will connect to
+    table_name: the name of the table that we will create
+
+for example, to read the file InputClosureTable.xlsx, connect to the niologic database, and create a table called CLOSURE_TABLE, we will use the following command
+
+    python closure_tables.py InputClosureTable.xlsx niologic CLOSURE_TABLE
+
+We assume that the Excel file will be in the same path as the script. 
+Both the database file and the text file that the script creates, will also be created in the same path as the script.
+"""
 
 ## LIBRARIES AND FUNCTIONS ##
 
@@ -13,22 +25,33 @@ import sqlite3
 import pandas as pd
 import sys
 
+
+def append_endpoints(row):
+    # used to append the start points and end points to the "paths" variable
+    paths.append(row.tolist())
+
+
 def extract_paths(dataframe):
-    paths = []
+    # loops over the hierarchy table, and picks out the start points and end points of paths in the hierarchy graph
     rows = dataframe.shape[0]
     columns = dataframe.shape[1]
     
     for i in range(columns):
         for j in range(i,columns):
-            for k in range(rows):
-                paths.append(dataframe.iloc[k,[i,j]].tolist())
-    
-    # we convert the paths list to a dataframe, in order to remove duplicates
+            x = dataframe.iloc[:,[i,j]]
+            x.apply(append_endpoints, axis=1)
+            
+    return paths
+
+
+def remove_duplicates(paths):
+    # we convert the "paths" list to a dataframe, in order to remove duplicates
     paths_df = pd.DataFrame(paths)
     paths_df.drop_duplicates(inplace=True, keep='first')
+
     # we then convert it back to a list, because that is the type that the executemany method expects
     paths = paths_df.values.tolist()
-    
+
     return paths
 
 
@@ -37,6 +60,9 @@ def extract_paths(dataframe):
 file_name = sys.argv[1]
 db_name = sys.argv[2]
 table_name = sys.argv[3]
+
+# the paths that we extract from the hierarchy graph will be stored here, as lists of length 2
+paths = []
 
 
 ## READ FILE ## 
@@ -47,6 +73,7 @@ data = pd.read_excel(file_name)
 ## EXTRACT PATHS ##
 
 paths = extract_paths(data)
+paths = remove_duplicates(paths)
 
 
 ## CONNECT TO DB ## 
